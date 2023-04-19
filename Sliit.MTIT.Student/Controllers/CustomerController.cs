a4d2e1891b467fc29cebced86187f6ef0c417b27
kasun <kasun@DESKTOP-GCGTP8D>
2023-04-19 20:53:22 +0530

    Add project files.

---
+++ Sliit.MTIT.Student/Controllers/CustomerController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+using Sliit.MTIT.Customer.Services;
+using Sliit.MTIT.Customer.Models;
+using Sliit.MTIT.Customer.Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Sliit.MTIT.Customer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerService _customerService;
+
+        public CustomerController(ICustomerService customerService) 
+        {
+            _customerService = customerService ?? throw new ArgumentNullException(nameof(customerService));
+        }
+
+        //get all customer
+        // GET: api/<StudentController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_customerService.GetCustomers());
+        }
+
+        //get customer bu ID
+        // GET api/<CustomerController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return _customerService.GetCustomer(id) != null ? Ok(_customerService.GetCustomer(id)) : NoContent();
+        }
+
+        //add customer
+        // POST api/<CustomerController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Models.Customer customer)
+        {
+            return Ok(_customerService.AddCustomer(customer));
+        }
+
+        //update customer
+        // PUT api/<CustomerController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put([FromBody] Models.Customer customer)
+        {
+            return Ok(_customerService.UpdateCustomer(customer));
+        }
+
+        //delete customer with the passed ID
+        // DELETE api/<customerController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var result = _customerService.DeleteCustomer(id);
+
+            return result.HasValue & result == true ? Ok($"customer with ID:{id} got deleted successfully.") : BadRequest($"Unable to delete the customer with the ID:{id}.");
+        }
+    }
+}
---
+++ Sliit.MTIT.Student/Data/CustomerMockDataService.cs
@@ -0,0 +1,15 @@
+﻿namespace Sliit.MTIT.Customer.Data
+{
+    public static class CustomerMockDataService
+    {
+        public static List<Models.Customer> Customers = new List<Models.Customer>()
+        {
+            new Models.Customer { Id = 1, Name = "Mr.Kalum", Address = "123 Main St", Age = 35, requirment = "Need to create High level desgin logos"},
+            new Models.Customer { Id = 2, Name = "Ms.Henrry", Address = "456 Second Ave", Age = 55, requirment = "Need to create Banners"},
+            new Models.Customer { Id = 3, Name = "Mr.Sadruwan", Address = "789 Third St", Age = 21, requirment = "Cahnge the previos logos to new one"},
+            new Models.Customer { Id = 4, Name = "Ms.Nadeesha", Address = "321 Fourth Ave", Age = 24, requirment = "Need to create facebook campaign"},
+            new Models.Customer { Id = 5, Name = "Mr.Kasun", Address = "654 Fifth St", Age = 39, requirment = "Need to create High level desgin logos"},
+        };
+            
+    }
+}
